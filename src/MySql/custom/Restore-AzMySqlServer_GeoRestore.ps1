6fa16b4f942129938a131621da518f16dfd67ace
msJinLei <lei.jin@microsoft.com>
2020-03-20 22:12:50 +0800

    Remove profile

--- src/MySql/custom/Restore-AzMySqlServer_GeoRestore.ps1
+++ src/MySql/custom/Restore-AzMySqlServer_GeoRestore.ps1
@@ -13,11 +13,10 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------------
 
-function Restore-AzMySqlServerWithGeo {
-    [OutputType([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.IServer])]
+function Restore-AzMySqlServer_GeoRestore {
+    [OutputType([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.IServer])]
     [CmdletBinding(PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
     [Microsoft.Azure.PowerShell.Cmdlets.MySql.Description('Restore a server from an existing backup')]
-    [Microsoft.Azure.PowerShell.Cmdlets.MySql.Profile('latest-2019-04-30')]
     param(
         [Parameter(Mandatory, HelpMessage = 'The name of the server.')]
         [Alias('ServerName')]
@@ -32,7 +31,7 @@ function Restore-AzMySqlServerWithGeo {
 
         [Parameter(Mandatory, ValueFromPipeline, HelpMessage = 'The source server object to restore from.')]
         [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
-        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.IServer]
+        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.IServer]
         ${InputObject},
 
         [Parameter(HelpMessage = 'The location the resource resides in.')]
@@ -43,13 +42,18 @@ function Restore-AzMySqlServerWithGeo {
         [Parameter(HelpMessage = 'The name of the sku, typically, tier + family + cores, e.g. B_Gen4_1, GP_Gen5_8.')]
         [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
         [System.String]
-        ${SkuName},
+        ${Sku},
 
         [Parameter(HelpMessage = 'Application-specific metadata in the form of key-value pairs.')]
         [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
-        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Runtime.Info(PossibleTypes=([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.IServerForCreateTags]))]
+        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Runtime.Info(PossibleTypes=([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.IServerForCreateTags]))]
         [System.Collections.Hashtable]
         ${Tag},
+
+        [Parameter(Mandatory, HelpMessage = 'Use Geo mode to restore')]
+        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
+        [System.Management.Automation.SwitchParameter]
+        ${UseGeoRestore},
 
         [Parameter(HelpMessage = 'The credentials, account, tenant, and subscription used for communication with Azure.')]
         [Alias('AzureRMContext', 'AzureCredential')]
@@ -109,9 +113,9 @@ function Restore-AzMySqlServerWithGeo {
 
     process {
         try {
-          $Parameter = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.ServerForCreate]::new()
+          $Parameter = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.ServerForCreate]::new()
 
-          $Parameter.Property = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.ServerPropertiesForGeoRestore]::new()
+          $Parameter.Property = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.ServerPropertiesForGeoRestore]::new()
           $Parameter.CreateMode = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Support.CreateMode]::GeoRestore
 
           $server = $PSBoundParameters['InputObject']
@@ -124,9 +128,9 @@ function Restore-AzMySqlServerWithGeo {
               $null = $PSBoundParameters.Remove('Location')
           }
 
-          if ($PSBoundParameters.ContainsKey('SkuName')) {
-              $Parameter.SkuName = $PSBoundParameters['SkuName']
-              $null = $PSBoundParameters.Remove('SkuName')
+          if ($PSBoundParameters.ContainsKey('Sku')) {
+              $Parameter.SkuName = $PSBoundParameters['Sku']
+              $null = $PSBoundParameters.Remove('Sku')
           }
 
           if ($PSBoundParameters.ContainsKey('Tag')) {
--- src/MySql/custom/Restore-AzMySqlServer_PointInTimeRestore.ps1
+++ src/MySql/custom/Restore-AzMySqlServer_PointInTimeRestore.ps1
@@ -13,11 +13,10 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------------
 
-function Restore-AzMySqlServer {
-    [OutputType([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.IServer])]
+function Restore-AzMySqlServer_PointInTimeRestore {
+    [OutputType([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.IServer])]
     [CmdletBinding(PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
     [Microsoft.Azure.PowerShell.Cmdlets.MySql.Description('Restore a server from an existing backup')]
-    [Microsoft.Azure.PowerShell.Cmdlets.MySql.Profile('latest-2019-04-30')]
     param(
         [Parameter(Mandatory, HelpMessage = 'The name of the server.')]
         [Alias('ServerName')]
@@ -37,7 +36,7 @@ function Restore-AzMySqlServer {
 
         [Parameter(Mandatory, ValueFromPipeline, HelpMessage = 'The source server object to restore from.')]
         [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
-        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.IServer]
+        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.IServer]
         ${InputObject},
 
         [Parameter(HelpMessage = 'The location the resource resides in.')]
@@ -48,13 +47,18 @@ function Restore-AzMySqlServer {
         [Parameter(HelpMessage = 'The name of the sku, typically, tier + family + cores, e.g. B_Gen4_1, GP_Gen5_8.')]
         [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
         [System.String]
-        ${SkuName},
+        ${Sku},
 
         [Parameter(HelpMessage = 'Application-specific metadata in the form of key-value pairs.')]
         [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
-        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Runtime.Info(PossibleTypes=([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.IServerForCreateTags]))]
+        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Runtime.Info(PossibleTypes=([Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.IServerForCreateTags]))]
         [System.Collections.Hashtable]
         ${Tag},
+
+        [Parameter(Mandatory, HelpMessage = 'Use PointInTime mode to restore')]
+        [Microsoft.Azure.PowerShell.Cmdlets.MySql.Category('Body')]
+        [System.Management.Automation.SwitchParameter]
+        ${UsePointInTimeRestore},
 
         [Parameter(HelpMessage = 'The credentials, account, tenant, and subscription used for communication with Azure.')]
         [Alias('AzureRMContext', 'AzureCredential')]
@@ -114,9 +118,9 @@ function Restore-AzMySqlServer {
 
     process {
         try {
-          $Parameter = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.ServerForCreate]::new()
+          $Parameter = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.ServerForCreate]::new()
 
-          $Parameter.Property = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201Preview.ServerPropertiesForRestore]::new()
+          $Parameter.Property = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Models.Api20171201.ServerPropertiesForRestore]::new()
           $Parameter.Property.RestorePointInTime = $PSBoundParameters['RestorePointInTime']
           $null = $PSBoundParameters.Remove('RestorePointInTime')
           $Parameter.CreateMode = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Support.CreateMode]::PointInTimeRestore
@@ -131,9 +135,9 @@ function Restore-AzMySqlServer {
               $null = $PSBoundParameters.Remove('Location')
           }
 
-          if ($PSBoundParameters.ContainsKey('SkuName')) {
-              $Parameter.SkuName = $PSBoundParameters['SkuName']
-              $null = $PSBoundParameters.Remove('SkuName')
+          if ($PSBoundParameters.ContainsKey('Sku')) {
+              $Parameter.SkuName = $PSBoundParameters['Sku']
+              $null = $PSBoundParameters.Remove('Sku')
           }
 
           if ($PSBoundParameters.ContainsKey('Tag')) {
